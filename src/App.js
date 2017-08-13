b4d686daf6c7d5cf847a59f48d0e1fd0f7c379df
Simon Mueller <13031695@student.uts.edu.au>
2017-08-14 01:53:44 +1000

    add yarn lock and add data fetch

--- src/App.js
+++ src/App.js
@@ -2,70 +2,112 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-require('isomorphic-fetch');
-require('es6-promise').polyfill();
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      releaseDate: '',
+      genre: '',
+      duration: 0,
+      synopsis: ''
+    };
 
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-class Movie extends Component {
-    constructor(props) {
-        super(props);
-        this.title = 'Default';
-        this.release = '01/01/1970';
-        this.duration = '42min';
-        this.genre = 'None';
-        this.synopsis = 'Description'; 
-    }
+  componentDidMount() {
+    var that = this;
+    var url = 'http://localhost:3001'
+
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        that.setState(data);
+      });
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    var url = 'http://localhost:3001'
+    var that = this;
+
+    event.preventDefault();
+    fetch(url, {
+      method: 'post',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(that.state)
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Title:
+          <input
+            name="title"
+            type="text"
+            value={this.state.title}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Release date:
+          <input
+            name="releaseDate"
+            type="text"
+            value={this.state.releaseDate}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Duration:
+          <input
+            name="duration"
+            type="number"
+            value={this.state.duration}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Genre:
+          <input
+            name="genre"
+            type="text"
+            value={this.state.genre}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Synopsis:
+          <input
+            name="synopsis"
+            type="text"
+            value={this.state.synopsis}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-
 class App extends Component {
-
-    constructor(props) {
-        super(props);
-        
-        this.movie = new Movie();
-        this.getJSONdata(this.movie);
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    /**
-        Get JSON movie from server
-    **/
-    getJSONdata(movie) {
-        //TODO: get movie object from server
-        movie.title = 'Killing Floor';
-        movie.release = '23/07/2018';
-        movie.duration = '90min';
-        movie.genre = 'Action';
-        movie.synopsis = 'Awesome movie.';
-        
-        fetch('/users.json').then(function(response) {
-            return response.json()
-        })
-        
-        return movie;
-    }
-    
-    /**
-        Send altered movie to server in JSON
-    **/
-    setJSONdata(event) {
-        alert(event.target.value);
-        this.movie.title = event.target.value;
-        //TODO: save new movie object to server
-    }
-  
-  
-    /**
-        Handle submit button event
-    **/
-    handleSubmit(event) {
-        alert(this.movie.title);
-        event.preventDefault();
-    }  
-
-    render() {
+  render() {
     return (
       <div className="App">
         <div className="App-header">
@@ -75,22 +117,7 @@ class App extends Component {
         <p className="App-intro">
             <b>This is a very serious project assignment.</b><br></br><br></br>
         </p>
-        <form onSubmit={this.handleSubmit}>
-        
-            Title:<input type="text" name="title" 
-                value={this.movie.title} onChange={this.setJSONdata}/>
-            <br></br>
-            Release date:<input type="text" name="release" value={this.movie.release}/>
-            <br></br>
-            Duration:<input type="text" name="duration" value={this.movie.duration}/>
-            <br></br>
-            Genre:<input type="text" name="genre" value={this.movie.genre}/>
-            <br></br>
-            Synopsis:<input type="text" name="synopsis" value={this.movie.synopsis}/>
-            <br></br>
-            <input type="submit" value="Submit" />
-            <br></br>
-        </form>
+        <Movie />
       </div>
     );
   }
