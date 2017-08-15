6c53dcbc14e2092715b25626a03c61f70c1b68be
Tobias Huonder <tobias.c.huonder@student.uts.edu.au>
2017-08-15 12:19:06 +1000

    split MovieList component to seperate file

--- src/App.js
+++ src/App.js
@@ -2,109 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Movie extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      releaseDate: '',
-      genre: '',
-      duration: 0,
-      synopsis: ''
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidMount() {
-    var that = this;
-    var url = 'http://localhost:3001'
-
-    fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        that.setState(data);
-      });
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit(event) {
-    var url = 'http://localhost:3001'
-    var that = this;
-
-    event.preventDefault();
-    fetch(url, {
-      method: 'post',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(that.state)
-    });
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Title:
-          <input
-            name="title"
-            type="text"
-            value={this.state.title}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Release date:
-          <input
-            name="releaseDate"
-            type="text"
-            value={this.state.releaseDate}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Duration:
-          <input
-            name="duration"
-            type="number"
-            value={this.state.duration}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Genre:
-          <input
-            name="genre"
-            type="text"
-            value={this.state.genre}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Synopsis:
-          <input
-            name="synopsis"
-            type="text"
-            value={this.state.synopsis}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+//Module requires
+var MovieList = require('./components/MovieList')
 
 class App extends Component {
   render() {
@@ -117,7 +16,7 @@ class App extends Component {
         <p className="App-intro">
             <b>This is a very serious project assignment.</b><br></br><br></br>
         </p>
-        <Movie />
+        <MovieList />
       </div>
     );
   }
---
+++ src/components/MovieList.js
@@ -0,0 +1,108 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+class MovieList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      releaseDate: '',
+      genre: '',
+      duration: 0,
+      synopsis: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    var that = this;
+    var url = 'http://localhost:3001'
+
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        that.setState(data);
+      });
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    var url = 'http://localhost:3001'
+    var that = this;
+
+    event.preventDefault();
+    fetch(url, {
+      method: 'post',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(that.state)
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Title:
+          <input
+            name="title"
+            type="text"
+            value={this.state.title}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Release date:
+          <input
+            name="releaseDate"
+            type="text"
+            value={this.state.releaseDate}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Duration:
+          <input
+            name="duration"
+            type="number"
+            value={this.state.duration}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Genre:
+          <input
+            name="genre"
+            type="text"
+            value={this.state.genre}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Synopsis:
+          <input
+            name="synopsis"
+            type="text"
+            value={this.state.synopsis}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+module.exports = MovieList
